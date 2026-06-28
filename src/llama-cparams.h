3c6f35cb949f13f31a54e48a6a6ccdb9cdec09db
anon_ai_developer <anon_ai_developer@message_me_on_github.com>
2026-06-27 23:26:31 -0400

    I forgot a file for the graph callback update

--- src/llama-cparams.h
+++ src/llama-cparams.h
@@ -38,4 +38,7 @@ struct llama_cparams {
 
     ggml_backend_sched_eval_callback cb_eval;
     void * cb_eval_user_data;
+
+    ggml_backend_sched_graph_callback cb_graph;
+    void * cb_graph_user_data;
 };
